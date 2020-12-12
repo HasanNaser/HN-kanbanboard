239e08606f10d69461ec971e9eb9f7eb6b7cc011
HasanNaser <19hasan0@gmail.com>
2020-12-12 23:17:30 +0300

    add some methods

--- demo.js
+++ demo.js
@@ -1,79 +1,94 @@
 
-let log = console.log;
+
+let lists = [{"id": "todo", "title": "Todo"}, {"id": "doing", "title": "Doing"}, {"id": "done", "title": "Done"}]
+
+let cards_ = [
+    {
+        "board_id": "todo",
+        "card_id": "1",
+        "card_title": "Card Drag/drop feature"
+    },
+    {
+        "board_id": "todo",
+        "card_id": "2",
+        "card_title": "onCardclick function"
+    },
+    {
+        "board_id": "done",
+        "card_id": "4",
+        "card_title": "setCards function"
+    },
+    {
+        "board_id": "done",
+        "card_id": "5",
+        "card_title": "onCardAdded function"
+    },
+    {
+        "board_id": "done",
+        "card_id": "8",
+        "card_title": "add new card feature"
+    },
+    {
+        "board_id": "done",
+        "card_id": "34",
+        "card_title": ".........."
+    },
+];
 
 var options = {
-    container : "#myDiv",
-    boards : {
-         0:{
-             "id":"1",
-             "title":"To do"
-         },
-         1:{
-            "id":"2",
-            "title":"Doing"
-        },
-        2:{
-            "id":"3",
-            "title":"Done"
-        },
-    },
-    cards :{
-        0:{
-             "board_id":"1",
-             "card_id":"1",
-             "card_title":"Card Drag/drop feature"
-         },
-        1:{
-            "board_id":"1",
-            "card_id":"2",
-            "card_title":"onCardclick function"
-        }, 
-        2:{
-            "board_id":"3",
-            "card_id":"4",
-            "card_title":"setCards function"
-        }, 
-        3:{
-            "board_id":"3",
-            "card_id":"5",
-            "card_title":"onCardAdded function"
-        }, 
-        4:{
-            "board_id":"3",
-            "card_id":"8",
-            "card_title":"add new card feature"
-        }, 
-        5:{
-            "board_id":"3",
-            "card_id":"34",
-            "card_title":".........."
-        }, 
-    },
-    onCardAdd : function(info){
-        log(info);
-    },
-    onCardDrgStart : function (evt){
-    },
-    onCardDrgEnd : function (evt){
-    //     console.log(evt.to,// target list
-    //     evt.from, // previous list
-    //     evt.oldIndex, // element's old index within old parent
-    //     evt.newIndex,// element's new index within new parent
-    //     evt.oldDraggableIndex, // element's old index within old parent, only counting draggable elements
-    //     evt.newDraggableIndex,// element's new index within new parent, only counting draggable elements
-    //     evt.clone, // the clone element
-    //     evt.pullMode,// when item is in another sortable: `"clone"` if cloning, `true` if moving
-    // )
-    },
-    onCardSortChange : function (evt){
+    container: "#myDiv",
 
-    },
-    onCardClick : function (val_attr){
-        log(val_attr);
+    lists: lists,
+    cards: cards_,
 
+    onCardAdd: function (data) {
+        //data.boardId,
+        //data.title,
+        console.info(data);
+    },
+    onCardDrgStart: function (evt) {
+        //evt.currentList   :current list
+        //evt.cardSelf      :card element
+        console.info(evt)
+    },
+    onCardDrgEnd: function (evt) {
+        //evt.listTo           : target list
+        //evt.boardFrom       : previous list
+        //evt.oldIndex        : element's old index within old parent
+        //evt.newInde         : element's new index within new parent
+        //evt.cardSelf        : card element
+        console.info(evt)
+    },
+    onCardSortChange: function (evt) {
+        //evt.currentList     :current list
+        //evt.oldIndex        : element's old index within old parent
+        //evt.newInde         : element's new index within new parent
+        //evt.cardSelf        : card element
+        console.info(evt)
+    },
+    onCardClick: function (data) {
+        //data.title,
+        //data.currentListId,
+        //data.currentList,
+        console.info(data);
     }
 }
 
+
 var nesne = HN_kanbanTable(options);
+
+
 nesne.init();
- 
+
+
+
+
+
+
+
+
+EnlighterJS.init('pre', 'code', {
+    language : 'javascript',
+    theme: 'atomic',
+    indent : 2
+});
--- dist/kanban.js
+++ dist/kanban.js
@@ -11,11 +11,12 @@
 
 
     let self = {};
-    self.boards = options.boards;
+    self.lists = options.lists;
     self.cards = options.cards;
     self.element = document.querySelector(options.container);
     self.cardTemp = '<div id="template" class="card" onclick=""><i id="windows_cover_saving" style="display:none;float: right; padding: 8px; font-size: 17px;" class="fa fa-spinner fa-spin"> </i> <div class="card-drag"> <div class="card-title">     </div> </div> </div>';
 
+    //Methods
     self.onCardAdd = options.onCardAdd;
     self.onCardDrgStart = options.onCardDrgStart;
     self.onCardDrgEnd = options.onCardDrgEnd;
@@ -24,58 +25,43 @@
 
 
 
-    let builtCards = (cards) => {
-      //clean up boards 
-      (document.querySelectorAll(".kanban-boards .board .card")).forEach(elem => elem.remove());
+    let builtBoards = (lists) => {
 
-      for (var key of Object.keys(cards)) {
-        let cardHTML = '<div data-id="' + cards[key].card_id + '" class="card" onclick=""> <div class="card-drag"> <div class="card-title">' + cards[key].card_title + ' </div> </div> </div>';
-        document.querySelector(".kanban-boards #board_" + cards[key].board_id + " .board-body").innerHTML += cardHTML;
+      var listsHTML = '<div class="kanban-boards">';
+      for (var key of Object.keys(lists)) {
+        listsHTML += '<div class="board" data-boardid=' + lists[key].id + '>';
+        listsHTML += '<div class="board-head">';
+        listsHTML += ' <p class="board-title">';
+        listsHTML += lists[key].title;
+        listsHTML += " </p>";
+        listsHTML += "</div>";
+        listsHTML += '<div class="board-body"></div> ';
+        listsHTML += '<div class="add_new_btn" style="display: block">';
+        listsHTML +=
+            ' <a style="padding: 9px;font-weight:   900; color: gray;"><i class="fa fa-plus"></i> &nbsp; Add Card</a>';
+        listsHTML += "</div>";
+        listsHTML += "</div>";
       }
-      (document.querySelectorAll(".board .card")).forEach(elem => elem.addEventListener("click", function (){
-        self.onCardClick(this.getAttribute("data-id"));
-      }));
+      listsHTML += "</div>";
 
-
-    }
-
-    let builtBoards = (boards) => {
-      
-      var boardsHTML = '<div class="kanban-boards">';
-      for (var key of Object.keys(boards)) {
-        boardsHTML += '<div class="board" id="board_' + boards[key].id + '">';
-        boardsHTML += '<div class="board-head">';
-        boardsHTML += ' <p class="board-title">';
-        boardsHTML += boards[key].title;
-        boardsHTML += " </p>";
-        boardsHTML += "</div>";
-        boardsHTML += '<div class="board-body"></div> ';
-        boardsHTML += '<div class="add_new_btn" style="display: block">';
-        boardsHTML +=
-          ' <a style="padding: 9px;font-weight:   900; color: gray;"><i class="fa fa-plus"></i> &nbsp; Add Card</a>';
-        boardsHTML += "</div>";
-        boardsHTML += "</div>";
-      }
-      boardsHTML += "</div>";
-
-      self.element.innerHTML = boardsHTML;
+      self.element.innerHTML = listsHTML;
 
       let prepareCardInput = function (element) {
         let currentCardCreater = document.querySelector(".kanban-boards .board #card_creater");
-        let boardId = this.parentElement.getAttribute("id");
+        let listId = this.parentElement.getAttribute("data-boardid");
         let templateCardCreater = '<div id="card_creater" class="task_card">' +
-          '<input type="text" class="card-input" autocomplete="off" placeholder="Type something">' +
-          '<small style="display:block;padding: 1px 0 5px 12px;color: #5f5f5f96;font-size: 11px;"> Press Enter to save</small>' +
-          '</div>';
+            '<input type="text" class="card-input" autocomplete="off" placeholder="Type something">' +
+            '<small style="display:block;padding: 1px 0 5px 12px;color: #5f5f5f96;font-size: 11px;"> Press Enter to save</small>' +
+            '</div>';
 
-        //clean up boards 
+        //clean up boards
         (document.querySelectorAll(".kanban-boards .board #card_creater")).forEach(elem => elem.remove());
 
 
         if (currentCardCreater == null)
-          document.querySelector(".kanban-boards #" + boardId + " .board-body").innerHTML += templateCardCreater;
+          document.querySelector(".kanban-boards [data-boardid=" + listId + "]  .board-body").innerHTML += templateCardCreater;
         else
-          document.querySelector(".kanban-boards #" + boardId + " .board-body").appendChild(currentCardCreater);
+          document.querySelector(".kanban-boards [data-boardid=" + listId + "]  .board-body").appendChild(currentCardCreater);
 
         document.querySelector(".kanban-boards #card_creater input").focus();
 
@@ -85,7 +71,8 @@
           if (pressedKey == 13) {
             let info = {};
             info.title = this.value;
-            info.boardId = this.parentNode.parentNode.parentNode.id;
+            info.currentListId = this.parentNode.parentNode.parentNode.getAttribute("data-boardid");
+            info.currentList = this.parentNode.parentNode.parentNode;
             self.onCardAdd(info);
           }
 
@@ -103,11 +90,31 @@
       var options = {
         group: 'share',
         animation: 150,
-        onStart : self.onCardDrgStart,
-        onEnd: self.onCardDrgEnd,  // Element dragging ended
-        onUpdate : self.onCardSortChange, // Changed sorting within list
+        onStart : function(evt){
+          let infos_ = {};
+          infos_.currentList =evt.from.parentNode; 
+          infos_.cardSelf = evt.item;
+          self.onCardDrgStart(infos_);
+        },
+        onEnd: function (evt){
+           let infos_ = {};
+           infos_.listFrom =evt.from.parentNode;
+           infos_.listTo =evt.to.parentNode;
+           infos_.oldIndex = evt.oldIndex;
+           infos_.newIndex = evt.newIndex;
+           infos_.cardSelf = evt.item;
+           self.onCardDrgEnd(infos_);
+        },  // Element dragging ended
+        onUpdate :function (evt){
+          let infos_ = {};
+          infos_.currentList =evt.from.parentNode;
+          infos_.oldIndex = evt.oldIndex;
+          infos_.newIndex = evt.newIndex;
+          infos_.cardSelf = evt.item;
+          self.onCardSortChange(infos_);
+        } , // Changed sorting within list
         store: {
-           // Get the order of elements. Called once during initialization.
+          // Get the order of elements. Called once during initialization.
           get: function (sortable) {
             var order = localStorage.getItem(sortable.options.group.name);
             return order ? order.split("|") : [];
@@ -119,19 +126,37 @@
           },
         },
       };
-      for (var key of Object.keys(boards)) {
-        Sortable.create(document.querySelector("#board_" + boards[key].id + " .board-body"), options);
+      for (var key of Object.keys(lists)) {
+        Sortable.create(document.querySelector(".kanban-boards  [data-boardid=" + lists[key].id + "] .board-body"), options);
       }
 
-      // var options1 = {
-      //   animation: 150,
-      // };
-      // Sortable.create(document.querySelector(".kanban-boards"), options1);
+      var options1 = {
+        animation: 150,
+      };
+      Sortable.create(document.querySelector(".kanban-boards"), options1);
 
     };
 
-    let setBoards = (boards) => {
-      self.boards = boards;
+    let builtCards = (cards) => {
+      //clean up boards 
+      (document.querySelectorAll(".kanban-boards .board .card")).forEach(elem => elem.remove());
+
+      for (var key of Object.keys(cards)) {
+        let cardHTML = '<div data-id="' + cards[key].card_id + '" class="card" onclick=""> <div class="card-drag"> <div class="card-title">' + cards[key].card_title + ' </div> </div> </div>';
+        let ggg = ".kanban-boards [data-boardid='" + cards[key].board_id + "'] .board-body";
+        document.querySelector(".kanban-boards [data-boardid='" + cards[key].board_id + "'] .board-body").innerHTML += cardHTML;
+      }
+      (document.querySelectorAll(".board .card")).forEach(elem => elem.addEventListener("click", function (){
+        let infos_ = {};
+        infos_.cardId =this.getAttribute("data-id");
+        infos_.cardSelf = this;
+        infos_.currentList =this.parentNode.parentNode;
+        self.onCardClick(infos_);
+      }));
+    }
+
+    let setBoards = (lists) => {
+      self.lists = lists;
       init();
     };
 
@@ -142,7 +167,7 @@
 
 
     var init = function () {
-      builtBoards(self.boards);
+      builtBoards(self.lists);
     };
 
 
